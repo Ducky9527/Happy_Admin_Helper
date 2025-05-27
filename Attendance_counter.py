88e557eecc81add0a54ac63dd73dd8b6b6cf18ba
Ducky <107194777+Ducky9527@users.noreply.github.com>
2025-05-27 22:22:26 +0930

    Update Attendance_counter.py
    add score column 
    add no_show

--- Attendance_counter.py
+++ Attendance_counter.py
@@ -39,7 +39,20 @@ axis=1 gives a row-wise sum
 
 Here, if I change the numner from 1 to 0, I will get a 'column-wise sum'.
 There's no axis=2.
+"""
 
+df['Score'] = df['Attendance'].map(lambda x: 10 if x >=5 else x * 2) # create a column for attendance score; 1 for 2 points, and max 10 from attendance.
+
+"""
+lambda x:10 if x >=5 else x *2
+
+def f(x):
+    if x >= 5:
+        score = 10
+    else:
+        score = x *2
+
+    return score
 """
 
 df.to_csv('Final_attendance.csv', index=False)
@@ -52,6 +65,7 @@ Second part of the code - in case you care how many students attended the tutori
 df = pd.read_csv('Admin/Final_attendance.csv')
 
 num_std = (df['Attendance'] > 5).sum() #feel free to change the number 5 into any number
+no_show = (df['Attendance'] == 0).sum() #count how many have never shown up in tutorials
 
 """
 .sum() = .sum(axis=0)
@@ -59,4 +73,4 @@ Which means 'column-wise sum.
 """
 
 print(num_std) # the result will be shown in your terminal.
-
+print(no_show)
